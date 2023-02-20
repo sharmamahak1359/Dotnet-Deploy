23a1e847d6277229337ad8393e7b25513c9e6bf4
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-20 18:58:40 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,69 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "3.41.0"
+    }
+  }
+}
+provider "azurerm" {
+    features {
+      
+    }
+}
+terraform {
+  backend "azurerm"{
+      resource_group_name = "tfrgblobstore"
+      storage_account_name = "tfstaccbackend"
+      container_name = "tfstate"
+      key = "terraform.tfstate"
+
+     }
+  }
+
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name     = "msrgiac"
+  location = "uksouth"
+}
+
+# Create an Azure Container Registry
+resource "azurerm_container_registry" "azurecontainerregistry" {
+  name                     = "msazurecg"
+  resource_group_name      = azurerm_resource_group.resourcegroup.name
+  location                 = azurerm_resource_group.resourcegroup.location
+  sku                      = "Standard"
+  admin_enabled            = true
+}
+
+# Create an App Service Plan
+resource "azurerm_app_service_plan" "msappserviceplan" {
+  name                = "msaspiac"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "msappservice" {
+  name                = "mssharmaasiac"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  app_service_plan_id = azurerm_app_service_plan.msappserviceplan.id
+
+  site_config {
+    dotnet_framework_version = "v5.0"
+    always_on                = true
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"         = azurerm_container_registry.azurecontainerregistry.login_server
+    "DOCKER_REGISTRY_SERVER_USERNAME"    = azurerm_container_registry.azurecontainerregistry.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.azurecontainerregistry.admin_password
+    "WEBSITES_PORT"                      = "80"
+  }
+}
